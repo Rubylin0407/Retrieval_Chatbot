57524a7dedd99beaa4debde0c38620176c8fba40
Rubylin0407 <rubylin0407@gmail.com>
2023-10-05 15:27:22 +0800

    insert all unique url from crawler into mongodb

--- crawler/database.py
+++ crawler/database.py
@@ -9,17 +9,28 @@ mongo_url = os.getenv("MONGODB_URL")
 db_name = os.getenv("DB_NAME")
 client = pymongo.MongoClient(mongo_url)
 
-def db_connection():
+def get_db_connection():
     # Connect to MongoDB
     db = client[db_name]
     return db
 
-def insert_into_mongodb(crawled_data, collection):
+def insert_crawler_data_into_mongodb(crawled_data, collection):
     try:
-        collection.insert_one(crawled_data)
-        print(f"successfully insert crawler data for {collection.name}")
+        with client.start_session() as session:
+            collection.insert_many(crawled_data)
+        print(f"Successfully inserted crawler data into {collection.name}")
     except Exception as e:
         print(f"Failed to insert data into {collection.name}: {str(e)}")
+
+def insert_unique_url_into_mongodb(urls, collection):
+    for url in urls:
+        # Check if the URL already exists in the collection
+        existing_url = collection.find_one({'url': url['url']})
+        if existing_url is None:
+            # URL doesn't exist in the collection, insert it
+            collection.insert_one(url)
+            print(f"Inserted URL: {url['url']} into {collection.name}")
+        # else: not insert, because URL already exists in the collection
 
 # Close the MongoDB connection
 def close_mongodb_connection():
